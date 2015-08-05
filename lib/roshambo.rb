c3c848cdcb39c3398b19ecc0a7d4ec64ffd7265d
Evan Clough & Alexa Zeazas <amzeazas@comcast.net>
2015-08-05 16:05:02 -0700

    Generate random number between 0 and 1.

---
+++ lib/roshambo.rb
@@ -0,0 +1,5 @@
+class String
+  define_method(:roshambo) do
+    computer_choice = rand()
+  end
+end
--- spec/roshambo_spec.rb
+++ spec/roshambo_spec.rb
@@ -3,7 +3,8 @@ require('roshambo')
 
 describe('String#roshambo') do
   it('generates a random number between zero and one') do
-    expect(roshambo()).to be > 0
-    expect(roshambo()).to be < 1
+    expect("tacocat".roshambo()).to be > 0
+    expect("tacocat".roshambo()).to be < 1
   end
+  
 end
