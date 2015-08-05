1a3c54486321c485afcabb6236e0db8d24a9c196
Evan Clough & Alexa Zeazas <amzeazas@comcast.net>
2015-08-05 16:14:09 -0700

    Write spec for generating random computer choice.

--- spec/roshambo_spec.rb
+++ spec/roshambo_spec.rb
@@ -6,5 +6,17 @@ describe('String#roshambo') do
     expect("tacocat".roshambo()).to be > 0
     expect("tacocat".roshambo()).to be < 1
   end
-  
+
+  it('generates rock if a random number is between 0 and .33; scissors if between
+  .34 and .66; paper if greater than .67') do
+    rand_num = rand()
+    if rand_num < 0.34
+      expect("tacocat".roshambo()).to(eq("rock"))
+    elsif rand_num >= 0.34 && rand_num < 0.67
+      expect("tacocat".roshambo()).to(eq("paper"))
+    else
+      expect("tacocat".roshambo()).to(eq("scissors"))
+    end
+  end
+
 end
